7003371a6c7fe514c0c35283f3861837504160e0
gliatem <gliatem@gmail.com>
2022-05-10 00:35:35 +0200

    añadida conexión a base de datos

---
+++ University/UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDBContext: DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options): base(options)
+        {
+
+        }
+        // TODO: AÑADIR DBSETS (TABLAS )
+    }
+}
